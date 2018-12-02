0035dd9dd2caec61de7bbd81c66d4b9bd0962a9c
Dhruv Paranjpye <dhruv@Dhruvs-MacBook-Pro.local>
2018-12-01 19:56:40 -0500

    addded the latest python file

--- parking_detection.py
+++ parking_detection.py
@@ -5,7 +5,6 @@ import cv2
 from scipy.misc import imsave
 import os
 import city_segment
-import math
 
 
 def region_of_interest(img, vertices):
@@ -31,7 +30,7 @@ def get_lines(img, roi, param=[6,20,80,20,35]):
     min_length=param[3]
     max_gap=param[4]
     gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    cannyed_image = cv2.Canny(gray_image, 10, 60) #first we get the canny edge detected linesw
+    cannyed_image = cv2.Canny(gray_image, 10, 150) #first we get the canny edge detected linesw
     imsave("cannyed_image.png", cannyed_image)
     if type(roi) == type([]):
         cropped_image = mask_image(cannyed_image, region_of_interest(cannyed_image, np.array([roi],np.int32)))
@@ -61,48 +60,6 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=3):
     # Merge the image with the lines onto the original.
     img = cv2.addWeighted(img, 0.8, line_img, 1.0, 0.0)
     # Return the modified image.
-    left_line_x = []
-    left_line_y = []
-    right_line_x = []
-    right_line_y = []
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            slope = (y2 - y1) / (x2 - x1) # <-- Calculating the slope.
-            if math.fabs(slope) < 0.5: # <-- Only consider extreme slope
-                continue
-            if slope <= 0: # <-- If the slope is negative, left group.
-                left_line_x.extend([x1, x2])
-                left_line_y.extend([y1, y2])
-            else: # <-- Otherwise, right group.
-                right_line_x.extend([x1, x2])
-                right_line_y.extend([y1, y2])
-    min_y = img.shape[0] * (3 / 5) # <-- Just below the horizon
-    max_y = img.shape[0] # <-- The bottom of the image
-    poly_left = np.poly1d(np.polyfit(
-        left_line_y,
-        left_line_x,
-        deg=1
-    ))
-    left_x_start = int(poly_left(max_y))
-    left_x_end = int(poly_left(min_y))
-    poly_right = np.poly1d(np.polyfit(
-        right_line_y,
-        right_line_x,
-        deg=1
-    ))
-    right_x_start = int(poly_right(max_y))
-    right_x_end = int(poly_right(min_y))
-    line_image = draw_lines(
-        img,
-        [[
-            [left_x_start, max_y, left_x_end, min_y],
-            [right_x_start, max_y, right_x_end, min_y],
-        ]],
-        thickness=5,
-    )
-    plt.figure()
-    plt.imshow(line_image)
-    plt.show()
 
     return img
 
@@ -113,7 +70,7 @@ if __name__ == "__main__":
     #img=cv2.imread("parking_2.png")
     #height, width, channels = img.shape
     param=[6,0.01,100,50,35]
-    filen="parking_5.png"
+    filen="parking_4.png"
 
     """
     rho=rho,theta=angle,threshold=thresh,lines=np.array([]),minLineLength=min_length,maxLineGap=max_gap)
