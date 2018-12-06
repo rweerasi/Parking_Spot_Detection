a9e6b8e052cc5387800d83960f5375b77fb86391
Rajith Weerasinghe <rweerasi@umich.edu>
2018-12-06 13:51:26 -0500

    Draw lines under all the cars

--- vehicle_segment.py
+++ vehicle_segment.py
@@ -6,6 +6,18 @@ import matplotlib.pyplot as plt
 from gluoncv import model_zoo, data, utils
 
 
+def cars_only(array, ids, scores, threshold):
+    #a = array
+    # return (np.array(a)[np.where(np.array(ids) == 2.), :])[0].tolist()
+    output = []
+    for ii, id in enumerate(ids):
+        # Check if the label is a car
+        if id[0] == 2. and scores[ii] > threshold:
+            output.append(array[ii])
+    return output
+
+
+
 def segmentation(filename, output_file="output.png"):
     """
     Input:
@@ -22,9 +34,16 @@ def segmentation(filename, output_file="output.png"):
     # Run network and store results
     ids, scores, bboxes, masks = [xx[0].asnumpy() for xx in net(x)]
 
-    # 
+    # Get only the cars
+    ids_car = cars_only(ids, ids, scores, 0.6)
+    scores_car = cars_only(scores, ids, scores, 0.6)
+    bboxes_car = cars_only(bboxes, ids, scores, 0.6 )
+    masks_car = cars_only(masks, ids, scores, 0.6)
+
+    # Formatting
     width, height = orig_img.shape[1], orig_img.shape[0]
-    masks = utils.viz.expand_mask(masks, bboxes, (width, height), scores)
+    """
+    masks = utils.viz.expand_mask(masks_car, bboxes_car, (width, height), scores_car)
     orig_img = utils.viz.plot_mask(orig_img, masks)
 
     # identical to Faster RCNN object detection
@@ -33,6 +52,19 @@ def segmentation(filename, output_file="output.png"):
     ax = utils.viz.plot_bbox(orig_img, bboxes, scores, ids,
                          class_names=net.classes, ax=ax)
     plt.show()
+    """
+
+    # Draw bottom bounding boxes on image
+    for bbox in bboxes_car:
+        x_1 = bbox[2]
+        x_2 = bbox[0]
+        y = max(bbox[1], bbox[3])
+        cv2.line(orig_img, (x_1, y), (x_2, y), (255, 0, 0), 5)
+    #cv2.imshow("", orig_img)
+    #cv2.waitKey(-1)
+
+    return bboxes_car
+
 
 if __name__ == "__main__":
     segmentation("example.jpg")
